d501587ac95ad66572a6a00cd4b1339252d10f36
paganellif <filippo1paganelli@gmail.com>
2024-01-10 19:00:02 +0100

    chore(ci): fix tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:gbemu/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Test home screen', (WidgetTester tester) async {
     await tester.pumpWidget(const GBEmu());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
+    expect(find.text('Start'), findsOneWidget);
+    expect(find.text('Select'), findsOneWidget);
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
